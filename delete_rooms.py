4d85637d17a026985803b653eb840f51401bce00
mpope9 <mpope@skillz.com>
2018-04-04 15:47:37 -0700

    [#155861133] Changing string split

--- delete_rooms.py
+++ delete_rooms.py
@@ -12,7 +12,6 @@ muc_rooms, stderr = all_rooms.communicate()
 
 # Loop through all muc rooms and delete them.
 for muc in muc_rooms.split(os.linesep):
-    name_service = muc.replace('@', ' ')
-    name_service_list = name_service.split(' ')
+    name_service_list = muc.split('@')
     if len(name_service_list) > 1:
         subprocess.Popen(['./sbin/ejabberdctl', 'destroy_room', name_service_list[0], name_service_list[1]])
