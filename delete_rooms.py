992d4ab41697722b228c4478d4d1048bd9e55f3d
mpope9 <mpope@skillz.com>
2018-04-04 14:17:16 -0700

    [#155861133] Tested and working batch room creation and deletion scripts

---
+++ delete_rooms.py
@@ -0,0 +1,19 @@
+# This should be ran from the default directory of the `ejabberd` user!
+
+import subprocess
+import os
+import io
+
+# Load all rooms with a - (dms and group messages) into a dictionary.
+from_ctl = subprocess.Popen(['./sbin/ejabberdctl', 'muc_online_rooms', 'global'], stdout=subprocess.PIPE)
+all_rooms = subprocess.Popen(['grep', '-'], stdin = from_ctl.stdout, stdout = subprocess.PIPE)
+from_ctl.stdout.close()
+
+muc_rooms, stderr = all_rooms.communicate()
+
+# Loop through all muc rooms and mark them as temporary then delete them.
+for muc in muc_rooms.split(os.linesep):
+    name_service = muc.replace('@', ' ')
+    name_service_list = name_service.split(' ')
+    if len(name_service_list) > 1:
+        subprocess.Popen(['./sbin/ejabberdctl', 'destroy_room', name_service_list[0], name_service_list[1]])
