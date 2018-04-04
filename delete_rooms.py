0b502adbdff43b788675963a480c1f32088fb95e
mpope9 <mpope@skillz.com>
2018-04-04 14:19:22 -0700

    [#155861133] Fixing comments

--- delete_rooms.py
+++ delete_rooms.py
@@ -4,14 +4,14 @@ import subprocess
 import os
 import io
 
-# Load all rooms with a - (dms and group messages) into a dictionary.
+# Load all rooms with a - (dms and group messages) 
 from_ctl = subprocess.Popen(['./sbin/ejabberdctl', 'muc_online_rooms', 'global'], stdout=subprocess.PIPE)
 all_rooms = subprocess.Popen(['grep', '-'], stdin = from_ctl.stdout, stdout = subprocess.PIPE)
 from_ctl.stdout.close()
 
 muc_rooms, stderr = all_rooms.communicate()
 
-# Loop through all muc rooms and mark them as temporary then delete them.
+# Loop through all muc rooms and delete them.
 for muc in muc_rooms.split(os.linesep):
     name_service = muc.replace('@', ' ')
     name_service_list = name_service.split(' ')
