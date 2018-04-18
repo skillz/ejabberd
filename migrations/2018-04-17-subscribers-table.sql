ab0fca6585f3d4f2d4add29af106154b7d13959c
mpope9 <mpope@skillz.com>
2018-04-18 16:50:21 -0700

    [#156709543] Removed the unique indexes

--- migrations/2018-04-17-subscribers-table.sql
+++ migrations/2018-04-17-subscribers-table.sql
@@ -7,5 +7,6 @@ CREATE TABLE subscriptions (
     subscription text NOT NULL,
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE UNIQUE INDEX i_subscriptions_subscription_index USING BTREE on subscriptions(subscription(25));
+CREATE INDEX i_subscriptions_name_index USING BTREE on subscriptions(name(25));
+CREATE INDEX i_subscriptions_subscription_index USING BTREE on subscriptions(subscription(25));
 CREATE UNIQUE INDEX i_subscriptions_created_at_index on subscriptions(created_at);
