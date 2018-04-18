0d0672931fba14c4391ac5efa869573294b7a6f8
mpope9 <mpope@skillz.com>
2018-04-18 16:03:36 -0700

    [#156709543] Adjusting the table

--- migrations/2018-04-17-subscribers-table.sql
+++ migrations/2018-04-17-subscribers-table.sql
@@ -4,8 +4,8 @@ USE ejabberd;
 
 CREATE TABLE subscriptions (
     name text NOT NULL,
-    subscriber_list mediumtext NOT NULL,
+    subscription text NOT NULL,
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE UNIQUE INDEX i_subscriber_name_index USING BTREE on subscriptions(name(25));
-CREATE UNIQUE INDEX i_subscriber_created_at_index on subscriptions(created_at);
+CREATE UNIQUE INDEX i_subscriptions_subscription_index USING BTREE on subscriptions(subscription(25));
+CREATE UNIQUE INDEX i_subscriptions_created_at_index on subscriptions(created_at);
