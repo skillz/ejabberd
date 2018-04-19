e56e0c69b8edcfc846e6d40fa2dec3bed7fe54af
mpope9 <mpope@skillz.com>
2018-04-19 08:35:42 -0700

    [#156709543] Fixing syntax errors

--- sql/2018-04-17-subscribers-table.sql
+++ sql/2018-04-17-subscribers-table.sql
@@ -3,10 +3,10 @@
 USE ejabberd;
 
 CREATE TABLE subscription (
-    name text NOT varchar(50),
+    name varchar(50) NOT NULL,
     subscription text NOT NULL,
     last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE INDEX i_subscriptions_name_index USING BTREE on subscriptions(name(25));
-CREATE INDEX i_subscriptions_subscription_index USING BTREE on subscriptions(subscription(25));
-CREATE INDEX i_subscriptions_created_at_index on subscriptions(created_at);
+CREATE INDEX i_subscription_name_index USING BTREE on subscription(name(25));
+CREATE INDEX i_subscription_subscription_index USING BTREE on subscription(subscription(25));
+CREATE INDEX i_subscription_last_updated_index on subscription(last_updated);
