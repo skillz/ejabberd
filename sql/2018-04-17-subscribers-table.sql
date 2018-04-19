b61b186541905b3bb392cf3e4d4d05e8749eaa4d
mpope9 <mpope@skillz.com>
2018-04-19 10:48:20 -0700

    [#156709543] Adjusting column names removing indexes

--- sql/2018-04-17-subscribers-table.sql
+++ sql/2018-04-17-subscribers-table.sql
@@ -1,14 +1,12 @@
--- This file creates a new table, subscriptions, in the ejabberd DB.
+-- This file creates a new table, subscription, in the ejabberd DB.
 
 USE ejabberd;
 
 CREATE TABLE subscription (
-    name varchar(50) NOT NULL,
-    subscription varchar(255) NOT NULL,
+    jid varchar(50) NOT NULL,
+    room varchar(255) NOT NULL,
     last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE INDEX i_subscription_name_index USING BTREE on subscription(name(25));
-CREATE INDEX i_subscription_subscription_index USING BTREE on subscription(subscription(25));
-CREATE INDEX i_subscription_last_updated_index on subscription(last_updated);
 CREATE INDEX i_subscription_created_at_index on subscription(created_at);
