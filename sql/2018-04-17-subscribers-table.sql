9166839177cadcd5d49c40dc349f8b730499fa13
mpope9 <mpope@skillz.com>
2018-04-19 10:53:33 -0700

    [#156709543] Fixing index

--- sql/2018-04-17-subscribers-table.sql
+++ sql/2018-04-17-subscribers-table.sql
@@ -8,5 +8,5 @@ CREATE TABLE subscription (
     last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE INDEX i_subscription_name_index USING BTREE on subscription(name(25));
+CREATE INDEX i_subscription_jid_index USING BTREE on subscription(jid(25));
 CREATE INDEX i_subscription_created_at_index on subscription(created_at);
