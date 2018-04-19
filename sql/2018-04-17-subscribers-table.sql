f1ec9c8de73475e6cd189f3558a7694a9dbf4436
mpope9 <mpope@skillz.com>
2018-04-19 08:27:38 -0700

    [#156709543] Fixing comments

--- sql/2018-04-17-subscribers-table.sql
+++ sql/2018-04-17-subscribers-table.sql
@@ -2,10 +2,10 @@
 
 USE ejabberd;
 
-CREATE TABLE subscriptions (
-    name text NOT NULL,
+CREATE TABLE subscription (
+    name text NOT varchar(50),
     subscription text NOT NULL,
-    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE INDEX i_subscriptions_name_index USING BTREE on subscriptions(name(25));
 CREATE INDEX i_subscriptions_subscription_index USING BTREE on subscriptions(subscription(25));
