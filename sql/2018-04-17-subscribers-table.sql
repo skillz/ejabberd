81e862ca5797e30f93f52c663d1f2aa71d8fc9bf
mpope9 <mpope@skillz.com>
2018-04-19 09:30:07 -0700

    [#156709543] Addressing more comments

--- sql/2018-04-17-subscribers-table.sql
+++ sql/2018-04-17-subscribers-table.sql
@@ -4,9 +4,11 @@ USE ejabberd;
 
 CREATE TABLE subscription (
     name varchar(50) NOT NULL,
-    subscription text NOT NULL,
-    last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+    subscription varchar(255) NOT NULL,
+    last_updated timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE INDEX i_subscription_name_index USING BTREE on subscription(name(25));
 CREATE INDEX i_subscription_subscription_index USING BTREE on subscription(subscription(25));
 CREATE INDEX i_subscription_last_updated_index on subscription(last_updated);
+CREATE INDEX i_subscription_created_at_index on subscription(created_at);
