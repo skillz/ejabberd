52709fb6d2708a6ac597154d458f845b3db529bd
Matthew Pope <mpope9@mail.csuchico.edu>
2018-10-11 09:51:25 -0700

    changing comment for clarity

--- include/message_type.hrl
+++ include/message_type.hrl
@@ -3,7 +3,7 @@
 %% This file contains message types that we shouldn't include in the 
 %% offline message flow.
 %%
-%% Also contains various constant that we need to access these items.
+%% Also contains various constant values that are the locations of stanza data.
 %%
 %%------------------------------------------------------------------------
 
