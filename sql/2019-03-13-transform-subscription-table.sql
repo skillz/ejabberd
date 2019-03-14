c2fbda958aedc25b685a27207e1bb4b71a93c283
Matthew Pope <mpope9@mail.csuchico.edu>
2019-03-14 08:28:27 -0700

    [ES-1694] Migration to move data from subscrption table

---
+++ sql/2019-03-13-transform-subscription-table.sql
@@ -0,0 +1,22 @@
+/**
+  * This is a SQL migration to move data from the subscription table to the muc_room_subscribers table.
+  *
+  **/
+
+/* Copy data. */
+INSERT INTO 
+    muc_room_subscribers (room, host, jid, nick, nodes, created_at)
+SELECT 
+    SUBSTRING_INDEX(room, '@', 1),
+    SUBSTRING_INDEX(room, '@', -1),
+    jid,
+    '[<<"urn:xmpp:mucsub:nodes:messages">>,
+      <<"urn:xmpp:mucsub:nodes:affiliations">>,<<"urn:xmpp:mucsub:nodes:subject">>,
+      <<"urn:xmpp:mucsub:nodes:config">>]',
+    created_at
+FROM 
+    subscription;
+
+/* Clear table of results. */
+TRUNCATE TABLE subscription;
+DROP     TABLE subscription;
