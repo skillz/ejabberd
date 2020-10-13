a12df44401e802c2d5ce2c8593d904adc6afcb3f
Brent Hickey <bhickey@skillz.com>
2020-10-13 11:16:09 -0700

    Revert "[PS-28889] Allow SDK to fetch privacy list v2 by reading/mapping values using v1 list"
    This reverts commit e5a0eae6bb93acb8e004d9f770c61252a8664810.

--- src/mod_privacy_sql.erl
+++ src/mod_privacy_sql.erl
@@ -342,17 +342,6 @@ get_privacy_list_id_t(LUser, LServer, Name) ->
       ?SQL("select @(id)d from privacy_list"
            " where username=%(LUser)s and %(LServer)H and name=%(Name)s")).
 
-get_privacy_list_data(LUser, LServer, "skillz_default_privacy_list_v2") ->
-    ejabberd_sql:sql_query(
-      LServer,
-      ?SQL("select @(t)s, @(value)s, @(action)s, @(ord)d, 0 as 'match_all', "
-           "0 as 'match_iq', 0 as 'match_message', @(match_presence_in)b, "
-           "@(match_presence_out)b from privacy_list_data "
-           "where id ="
-           " (select id from privacy_list"
-           " where username=%(LUser)s and %(LServer)H and name='skillz_default_privacy_list') "
-           "order by ord")).
-
 get_privacy_list_data(LUser, LServer, Name) ->
     ejabberd_sql:sql_query(
       LServer,
