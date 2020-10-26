1a9493756e4b5c0e597895fb2978a2ef7ce88ffc
Brent Hickey <bhickey@skillz.com>
2020-10-26 16:31:41 -0700

    [PS-28369] Send allow again to verify that spool is written to

--- src/mod_offline.erl
+++ src/mod_offline.erl
@@ -405,7 +405,7 @@ need_to_store(LServer, #message{type = Type} = Packet) ->
 store_packet({_Action, #message{from = From, to = To} = Packet} = Acc) ->
     case need_to_store(To#jid.lserver, Packet) of
 	true ->
-	    case ejabberd_hooks:run_fold(privacy_check_packet, To#jid.lserver, respect_mute, [To, Packet, in]) of
+	    case ejabberd_hooks:run_fold(privacy_check_packet, To#jid.lserver, allow, [To, Packet, in]) of
 	    	true ->
 			    case check_event(Packet) of
 				true ->
