39ab4664b545ebfe40834ed0aea1dc90309c19eb
dsills-skillz <devin@collegenet.com>
2022-02-14 16:29:47 -0800

    no host on blocking list; hit all nodes when using API for writes

--- src/mod_block_incoming.erl
+++ src/mod_block_incoming.erl
@@ -15,10 +15,11 @@
 -export([start/2, stop/1, reload/3, depends/2, mod_options/1, mod_opt_type/1]).
 
 %% gen_server callbacks.
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, get_proc_name/1]).
 
 %% ejabberd_commands callbacks.
--export([get_commands_spec/0, add_blocking_user/2, remove_blocking_user/2, get_blocking_users/1, reset_blocking_users/1, clear_blocking_users/1]).
+-export([get_commands_spec/0, add_blocking_user/2, remove_blocking_user/2, get_blocking_users/1, reset_blocking_users/1, clear_blocking_users/1,
+  add_blocking_user_all/2, remove_blocking_user_all/2, reset_blocking_users_all/1, clear_blocking_users_all/1]).
 
 %% hook handlers
 -export([filter_packet/1, filter_offline_msg/1, filter_subscription/2]).
@@ -181,9 +182,13 @@ is_blocking(Pkt) ->
       _ -> true
     end,
   IsFromActualUserOrRemoteServer = (From#jid.luser /= <<"">> orelse not ejabberd_router:is_my_host(From#jid.lserver)),
-  IsToUserBlockingIncoming = is_blocking_user(To#jid.lserver, jid:encode(To)),
+  IsToUserBlockingIncoming = is_blocking_user(To#jid.lserver, To#jid.luser),
+  IsFromUserBlockingIncoming = is_blocking_user(From#jid.lserver, From#jid.luser),
 
-  IsToAnotherUser andalso HasMessageContent andalso IsFromActualUserOrRemoteServer andalso IsToUserBlockingIncoming
+  %% if to someone else with content and from a real user and to-user is blocking and from user is not blocking, then block msg
+  %% basically we block if to-user is blocking and everything else is normal
+  %% also we do not block if both users are blocking bc both are then higher priv moderators and can be trusted
+  IsToAnotherUser andalso HasMessageContent andalso IsFromActualUserOrRemoteServer andalso IsToUserBlockingIncoming andalso not IsFromUserBlockingIncoming
 .
 
 maybe_adjust_from(#message{type = groupchat, from = From} = Msg) -> Msg#message{from = jid:remove_resource(From)};
@@ -221,16 +226,40 @@ is_blocking_user(Host, User) ->
   end
 .
 
+reset_blocking_users_all(Host) ->
+  lists:foreach(fun(Node) ->
+    rpc:call(Node, ?MODULE, reset_blocking_users, [Host], ?COMMAND_TIMEOUT)
+  end, ejabberd_admin:list_cluster())
+.
+
 reset_blocking_users(Host) ->
   gen_server:call(get_proc_name(Host), {reset_blocking_users}, ?COMMAND_TIMEOUT)
+.
+
+clear_blocking_users_all(Host) ->
+  lists:foreach(fun(Node) ->
+    rpc:call(Node, ?MODULE, clear_blocking_users, [Host], ?COMMAND_TIMEOUT)
+  end, ejabberd_admin:list_cluster())
 .
 
 clear_blocking_users(Host) ->
   gen_server:call(get_proc_name(Host), {clear_blocking_users}, ?COMMAND_TIMEOUT)
 .
 
+add_blocking_user_all(Host, User) ->
+  lists:foreach(fun(Node) ->
+    rpc:call(Node, ?MODULE, add_blocking_user, [Host, User], ?COMMAND_TIMEOUT)
+  end, ejabberd_admin:list_cluster())
+.
+
 add_blocking_user(Host, User) ->
   gen_server:call(get_proc_name(Host), {add_blocking_user, User}, ?COMMAND_TIMEOUT)
+.
+
+remove_blocking_user_all(Host, User) ->
+  lists:foreach(fun(Node) ->
+    rpc:call(Node, ?MODULE, remove_blocking_user, [Host, User], ?COMMAND_TIMEOUT)
+  end, ejabberd_admin:list_cluster())
 .
 
 remove_blocking_user(Host, User) ->
@@ -244,7 +273,7 @@ get_commands_spec() ->
       tags = [block],
       desc = "Add a user to the list of users blocking incoming subscriptions (friend) requests and unsolicited messages",
       module = ?MODULE,
-      function = add_blocking_user,
+      function = add_blocking_user_all,
       args = [{host, binary}, {jid, binary}],
       result = {res, rescode}
     },
@@ -254,7 +283,7 @@ get_commands_spec() ->
       tags = [block],
       desc = "Remove a user from the list of users blocking incoming subscriptions (friend) requests and unsolicited messages",
       module = ?MODULE,
-      function = remove_blocking_user,
+      function = remove_blocking_user_all,
       args = [{host, binary}, {jid, binary}],
       result = {res, rescode}
     },
@@ -264,7 +293,7 @@ get_commands_spec() ->
       tags = [block],
       desc = "Reset the list of blocking users to the initial module configuration",
       module = ?MODULE,
-      function = reset_blocking_users,
+      function = reset_blocking_users_all,
       args = [{host, binary}],
       result = {res, rescode}
     },
@@ -274,7 +303,7 @@ get_commands_spec() ->
       tags = [block],
       desc = "Clear the list of blocking users such that the list is empty",
       module = ?MODULE,
-      function = clear_blocking_users,
+      function = clear_blocking_users_all,
       args = [{host, binary}],
       result = {res, rescode}
     },
