02a93c6c849709151c0be66054b70739456a26ca
rohit-kakul <rkakulapati@skillz.com>
2026-03-03 14:43:16 +0530

    port muc funct

--- src/mod_muc_sql.erl
+++ src/mod_muc_sql.erl
@@ -244,7 +244,41 @@ forget_room(LServer, Host, Name) ->
     ejabberd_sql:sql_transaction(LServer, F).
 
 forget_rooms(LServer, Host, Rooms) ->
-    lists:foreach(fun(Name) -> forget_room(LServer, Host, Name) end, Rooms).
+    forget_rooms(LServer, Host, Rooms, length(Rooms), 32).
+
+forget_rooms(LServer, Host, Rooms, RoomsLen, BatchSize) ->
+    {BatchRooms, Rest} = case RoomsLen > BatchSize of
+			     true -> lists:split(BatchSize, Rooms);
+			     _ -> {Rooms, []}
+			 end,
+    forget_rooms_batch(LServer, Host, BatchRooms),
+    case Rest of
+	[] -> ok;
+	_ ->
+	    timer:sleep(30000),
+	    forget_rooms(LServer, Host, Rest, RoomsLen - BatchSize, BatchSize)
+    end.
+
+forget_rooms_batch(LServer, Host, Rooms) ->
+    case Rooms of
+	[] -> ok;
+	_ ->
+	    RoomInStr = str:join([[<<"'">>, ejabberd_sql:escape(Room), <<"'">>]
+				  || Room <- Rooms], <<",">>),
+	    RoomQuery = <<(<<"delete from muc_room where host = '">>)/binary,
+			 (ejabberd_sql:escape(Host))/binary, (<<"' and name in (">>)/binary,
+			 RoomInStr/binary,
+			 (<<")">>)/binary>>,
+	    RoomSubscrQuery = <<(<<"delete from muc_room_subscribers where host = '">>)/binary,
+				(ejabberd_sql:escape(Host))/binary, (<<"' and room in (">>)/binary,
+				RoomInStr/binary,
+				(<<")">>)/binary>>,
+	    F = fun () ->
+			ejabberd_sql:sql_query_t(RoomQuery),
+			ejabberd_sql:sql_query_t(RoomSubscrQuery)
+		end,
+	    ejabberd_sql:sql_transaction(LServer, F)
+    end.
 
 can_use_nick(LServer, ServiceOrRoom, JID, Nick) ->
     SJID = jid:encode(jid:tolower(jid:remove_resource(JID))),
